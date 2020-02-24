60bd0f12f8ab227e186c728d4d201dc5e3d3df10
Peter <petergorman11@gmail.com>
2020-02-23 23:04:36 -0500

    Add basic config.cpp

---
+++ Source/addons/501ccAuxMod/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+    authors[] = {"gunman435"};
+    url = "https://tamewater.com";
+
+    class 501ccAux
+    {
+        name = "501st Carnivore Company Aux Mod";
+        author = "gunman435";
+
+        requiredVersion = 0.1;
+        requiredAddons[] = {
+            ""
+        };
+
+        units[]={};
+        weapons[]={};
+    }
+};
+class cfgMod
+{
+    author = "gunman435";
+}
