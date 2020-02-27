1afef2a056eee39984e126d6042443807ccb008f
Peter <petergorman11@gmail.com>
2020-02-27 00:05:51 -0500

    Add 501ccMain and temporary logo

---
+++ Source/addons/501ccMain/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches
+{
+    class 501ccMain
+    {
+        name = "501st Carnivore Company Aux - Main";
+        author = "gunman435";
+
+        requiredVersion = 0.1;
+        requiredAddons[] = {};
+
+        units[]={};
+        weapons[]={};
+    };
+};
+
+class cfgMods
+{
+    author = "[TW] gunman435";
+};
