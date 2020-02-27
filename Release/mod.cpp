0838402e2edd7083cfc82bdfe37d87ad91bcc9d6
Peter <petergorman11@gmail.com>
2020-02-27 00:05:10 -0500

    Add mod.cpp

---
+++ Release/mod.cpp
@@ -0,0 +1,8 @@
+name = "501st Carnivore Company Auxiliary Mod";
+tooltipOwned="501st Carnivore Company Aux";
+picture = "501ccMain\logo.paa";
+logoSmall = "501ccMain\logo.paa";
+logo = "501ccMain\logo.paa";
+logoOver = "501ccMain\logo.paa";
+hideName = 0;
+hidePicture = 0;
