33720bf237f3080d8ce35912d1595ab9186a54fc
Peter <tamewater.gunman@gmail.com>
2020-03-05 10:58:56 -0500

    Create weapons PBO

---
+++ Source/addons/501ccWeapons/config.cpp
@@ -0,0 +1,20 @@
+class CfgPatches
+{
+    class 501ccWeapons
+    {
+        name = "501st Carnivore Company Aux - Weapons";
+        author = "gunman435";
+
+        requiredVersion = 0.1;
+        requiredAddons[] = {};
+
+        units[]={};
+        weapons[]={};
+    };
+};
+
+
+class cfgMods
+{
+    author = "[TW] gunman435";
+};
